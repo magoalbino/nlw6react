3ec9c7e88dbd1c251526192d496bfac95c7e9c68
Daniel <magoalbino@gmail.com>
2021-06-25 12:45:20 -0300

    aula 5

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,8 +1,12 @@
-
+import { Fragment } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
+
+import Modal from 'react-modal';
 
 import logoImg from '../assets/images/logo.svg';
 import deleteImg from '../assets/images/delete.svg';
+import checkImg from '../assets/images/check.svg';
+import answerImg from '../assets/images/answer.svg'; 
 
 import { Button } from '../components/Button';
 import { Question } from '../components/Question';
@@ -11,6 +15,7 @@ import { useRoom } from '../hooks/useRoom';
 
 import '../styles/room.scss';
 import { database } from '../services/firebase';
+import { useState } from 'react';
 
 type RoomParams = {
   id: string;
@@ -21,6 +26,8 @@ export function AdminRoom() {
   const roomId = params.id;
   const { title, questions  } = useRoom(roomId);
   const history = useHistory();
+  // const [isModalVisible, setIsModalVisible] = useState(false);
+  const [questionIdModalOpen, setQuestionIdModalOpen] = useState<string | undefined>();
 
   async function hanleEndRoom() {
     database.ref(`rooms/${roomId}`).update({
@@ -30,10 +37,30 @@ export function AdminRoom() {
     history.push('/');
   }
 
-  async function handleDeleteQuestion(questionId: string){
-    if (window.confirm('Tem certeza que você deseja excluir esta pergunta?')) {
-      await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
+  async function handleDeleteQuestion(questionId: string | undefined){
+
+    if(questionId === undefined) {
+      return;
     }
+    
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
+    
+    setQuestionIdModalOpen(undefined);
+    // if (window.confirm('Tem certeza que você deseja excluir esta pergunta?')) {
+    //   await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
+    // }
+  }
+
+  async function handleSetQuestionAsAnswered(questionId: string){
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isAnswered: true,
+    });
+  }
+
+  async function handleHighLightQuestion(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isHighlighted: true,
+    });
   }
 
   return (
@@ -58,23 +85,48 @@ export function AdminRoom() {
         {
           questions.map(question => {
             return (
-              <Question 
-                key={question.id}
-                content={question.content}
-                author={question.author}
-              >
-                <button
-                  type="button"
-                  onClick={() => handleDeleteQuestion(question.id)}
+              <Fragment key={question.id}>
+                <Question 
+                  key={question.id}
+                  content={question.content}
+                  author={question.author}
+                  isAnswered={question.isAnswered}
+                  isHighlighted={question.isHighlighted}
                 >
-                  <img src={deleteImg} alt="Remover pergunta" />
-                </button>
-              </Question>
+                  {!question.isAnswered && (<>
+                    <button
+                    type="button"
+                    onClick={() => handleSetQuestionAsAnswered(question.id)}
+                    >
+                      <img src={checkImg} alt="Marcar pergunta como respondida" />
+                    </button>
+                    <button
+                      type="button"
+                      onClick={() => handleHighLightQuestion(question.id)}
+                    >
+                      <img src={answerImg} alt="Dar destaque à pergunta" />
+                    </button>
+                  </>)}
+                  <button
+                    type="button"
+                    onClick={() => setQuestionIdModalOpen(question.id)}
+                  >
+                    <img src={deleteImg} alt="Remover pergunta" />
+                  </button>
+                </Question>                
+              </Fragment>
             );
           })
         }
         </div>
       </main>
+      <Modal 
+        isOpen={questionIdModalOpen !== undefined}
+        onRequestClose={() => setQuestionIdModalOpen(undefined)}
+      >
+        <button onClick={() => handleDeleteQuestion(questionIdModalOpen)}> Deletar </button>
+        <button onClick={() => setQuestionIdModalOpen(undefined)}> Fechar </button>
+      </Modal>
     </div>
   );
 }
